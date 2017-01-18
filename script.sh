3a1a4b6bfec4bfaba3090059d1b45ea50456529e
Peter Strøiman <peter@stroiman.com>
2017-01-18 10:53:41 +0100

    Optimize git checkout

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
-set -e # Automatically fail script when some command fail
-source ./install_deps.sh
+set -r # Automatically fail script when some command fail
+bash ./install_deps.sh 
 
 cd $HOME # Make sure we are in /home/vagrant
-git clone https://github.com/cloudfoundry/cf-release.git
+if [ ! -d $HOME/cf-release ]; then
+  git clone https://github.com/cloudfoundry/cf-release.git
+  git checkout v250
+else
+  echo "cf-release already checked out, skipping"
+fi
+
+
