da3d1b5c5a545c217de79f8cb2d0419fd9ab2a4b
Peter Strøiman <peter@stroiman.com>
2017-01-18 13:19:20 +0100

    Quit wget output
    Vagrant provisioning does not handle cursor positioning well, resulting
    in screen being filled with noise during download

--- install_spiff.sh
+++ install_spiff.sh
@@ -3,7 +3,7 @@ set -e
 if [ ! -f $HOME/bin/spiff ]; then
   mkdir -p $HOME/bin
   pushd $HOME/bin
-  wget https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.8/spiff_linux_amd64.zip
+  wget https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.8/spiff_linux_amd64.zip -q
   unzip spiff_linux_amd64.zip
   popd
 else
