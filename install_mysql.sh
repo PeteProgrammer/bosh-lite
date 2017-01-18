fec90b531b9d05e7398b498625cff2f53a5a33ae
Peter Strøiman <peter@stroiman.com>
2017-01-18 16:16:19 +0100

    Fixed line endings!

--- install_mysql.sh
+++ install_mysql.sh
@@ -2,8 +2,10 @@ if [ ! -d $HOME/cf-mysql-release ]; then
 	cd $HOME
 	git clone https://github.com/cloudfoundry/cf-mysql-release.git
 	cd $HOME/cf-mysql-release
-	./scripts/update
-	bosh upload release releases/cf-mysql/cf-mysql-32.yml
+	# ./scripts/update
+	git checkout v32
+	bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-mysql-release?v=32
+	# bosh upload release releases/cf-mysql/cf-mysql-32.yml
 	./scripts/generate-bosh-lite-manifest
 	echo "yes" | bosh deploy
 	bosh run errand broker-registrar
