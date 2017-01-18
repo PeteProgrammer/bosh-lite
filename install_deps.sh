fc7877ae1af5782a4e59b9358a6bc015fb0aaf03
Peter Strøiman <peter@stroiman.com>
2017-01-18 10:39:02 +0100

    Optimize apt-get updates

---
+++ install_deps.sh
@@ -0,0 +1,10 @@
+sudo apt-get install -y git
+
+# First time we run this, we need to update package repos,
+# so if installation fails, update package repos, and try
+# again.
+if [ $? -ne 0 ]; then
+  sudo apt-get update && sudo apt-get -y install git
+fi
+exit $?
+
--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get -y install git
+set -e # Automatically fail script when some command fail
+source ./install_deps.sh
 
-cd $HOME
+cd $HOME # Make sure we are in /home/vagrant
 git clone https://github.com/cloudfoundry/cf-release.git
